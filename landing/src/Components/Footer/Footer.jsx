1875115929df71c745a472fda8522e7d2377d334
MJephat <mukundijephat22@gmail.com>
2023-09-23 15:48:22 +0300

    footer

---
+++ landing/src/Components/Footer/Footer.jsx
@@ -0,0 +1,176 @@
+import React from 'react'
+import './footer.scss'
+import video4 from '../Assets/video4.mp4'
+import {FiSend} from 'react-icons/fi'
+import {MdOutlineTravelExplore} from 'react-icons/md'
+import {AiOutlineTwitter} from 'react-icons/ai'
+import {AiOutlineYoutube} from 'react-icons/ai'
+import {AiOutlineInstagram } from 'react-icons/ai'
+import {FaTripadvisor} from 'react-icons/fa'
+import {FiChevronRight} from 'react-icons/fi'
+
+
+export const Footer = () => {
+  return (
+    <section className='footer'>
+      <div className='videoDiv'>
+        <video src={video4} loop autoplay muted type='video/mp4'></video>
+      </div>
+
+      <div className='secContent container'>
+        <div className='contactDiv flex'>
+          <div className='text'>
+            <small> KEEP IN TOUCH </small>
+            <h2>Travel with us</h2>
+          </div>
+
+          <div className='inputDiv flex'>
+            <input type='text' placeholder='Enter Email Address'/>
+            <button className='btn flex' type='submit'>
+             SEND <FiSend className='icon'/>
+            </button>
+            </div>          
+        </div>
+
+        <div className='footerCard flex'>
+          <div className='footerIntro flex'>
+            <div className='logoDiv'>
+              <a href='#' className='logo flex'>
+                <MdOutlineTravelExplore className='icon'/>
+                Travel.
+              </a>
+            </div>
+
+            <div className='footerParagraph'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+            ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            </div>
+
+            <div className='footerSocials flex'>
+              <AiOutlineTwitter className='icon'/>
+              <AiOutlineInstagram className='icon'/>
+              <AiOutlineYoutube className='icon'/>
+              <FaTripadvisor className='icon'/>
+
+            </div>
+
+
+          </div>
+
+          <div className='footerLinks grid'>
+            {/* group one */}
+            <div className='linkGroup'>
+              <span className='groupTitle'>
+                OUR AGENCY
+              </span>
+
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Services
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Insurance
+              </li>
+             
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Agency
+              </li>
+              
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Tourism
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Payment
+              </li>
+
+
+            </div>
+
+             {/* group Two */}
+             <div className='linkGroup'>
+              <span className='groupTitle'>
+                PARTNERS
+              </span>
+
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Bookings
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Rentcars
+              </li>
+             
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                HostelWorld
+              </li>
+              
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Trivago
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                TripAdvisors
+              </li>
+
+
+            </div>
+
+             {/* group Three */}
+             <div className='linkGroup'>
+              <span className='groupTitle'>
+                LAST MUNITES
+              </span>
+
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Kenya
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Ethiopia
+              </li>
+             
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Tanzania
+              </li>
+              
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Uganda
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Rwanda
+              </li>
+
+
+            </div>
+
+          </div>
+
+          <div className='footerDiv flex'>
+              <small>BEST TRAVEL WEBSITE THEME</small>
+              <small>COPYRIGHT RESERVED - @jmm.2023</small>
+            </div>
+            
+        </div>
+      </div>
+    </section>
+  )
+}
